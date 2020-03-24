43e07f3badc8795ee4879dcb4ffe4af834b68a24
Xilefmusics <xilefmusics@gmail.com>
2020-03-24 17:42:19 +0100

    added concli mutt and nohtml

---
+++ source/nohtml/nohtml.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "html_entity.h"
+
+int main(int argc, char *argv[]) {
+    char c;
+    char buffer[10];
+    int buffer_index;
+    while ((c = getchar()) != EOF) {
+        if (c == '<') {
+            while ((c = getchar()) != EOF && c != '>') {}
+        } else if (c == '&') {
+            buffer_index = 0;
+            do {
+                buffer[buffer_index++] = c;
+            } while(buffer_index < 8 && (c = getchar()) != EOF && c != ';');
+            buffer[buffer_index++] = ';';
+            buffer[buffer_index++] = '\0';
+            for (int i = 0; i < 134; i++) {
+                if (!strcmp(buffer, htmlentity_to_charstrings[i].htmlentity)) {
+                    printf("%s", htmlentity_to_charstrings[i].charstring);
+                    break;
+                }
+            }
+        } else {
+            putchar(c);
+        }
+    }
+    return 0;
+}
