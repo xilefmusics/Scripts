9eb9c5b21812a5d0bf8eb68a3fa6175cc77b1060
Xilefmusics <xilefmusics@gmail.com>
2020-08-13 17:39:41 +0200

    xns works for A records and forwarding everything else

--- source/include/soclib.h
+++ source/include/soclib.h
@@ -6,17 +6,32 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int udp_socket_in(int port) {
-  struct sockaddr_in si_me;
-  int s;
-	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    return -1;
-	si_me.sin_family = AF_INET; // IPv4
-	si_me.sin_addr.s_addr = htonl(INADDR_ANY); // Ip-Adresse
-	si_me.sin_port = htons(port); // Port
-  if(bind(s, (struct sockaddr*)&si_me, sizeof(si_me)) == -1)
-    return -2;
-  return s;
+/**
+ * reads uint32 from char*
+ */
+char *write_uint16(char *ptr, uint16_t value){
+  ptr[0] = (char) (value>>8);
+  ptr[1] = (char) value;
+  return ptr+2;
+}
+
+/**
+ * writes uint32 to char*
+ */
+char *write_uint32(char *ptr, uint32_t value){
+  ptr[0] = (char) (value>>24);
+  ptr[1] = (char) (value>>16);
+  ptr[2] = (char) (value>>8);
+  ptr[3] = (char) value;
+  return ptr+4;
+}
+
+/**
+ * writes uint16 to char*
+ */
+char *read_uint16(char *ptr, uint16_t *value) {
+  *value = (((uint16_t)(ptr[0]))<<8) | (uint16_t)(ptr[1]);
+  return ptr+2;
 }
 
 /**
@@ -72,5 +87,36 @@ int udp_server(int port, char* in_buf, int in_buf_len, int (*handler)(char *in_b
     if (sendto(s, out_buf, len, 0, (struct sockaddr*) &si_other, slen) == -1)
       return -4;
   }
+  close(s);
   return 0;
+}
+
+/**
+ * creates simple udp request
+ *
+ * arguments:
+ *  - int socket
+ *  - int port (server)
+ *  - int ipv4 (server)
+ *  - char *out_buf
+ *  - int out_len
+ *  - char *in_buf
+ *  - int in_buf_len
+ *
+ *  returns:
+ *      -3: error while receiving request
+ *      -4: error while sending response
+ *    else: len of response
+ */
+int udp_request(int socket, int port, int ipv4, char* out_buf, int out_len, char* in_buf, int in_buf_len) {
+  struct sockaddr_in server_addr;
+  int in_len, slen = sizeof(server_addr);
+	server_addr.sin_family = AF_INET; // IPv4
+	server_addr.sin_addr.s_addr = htonl(ipv4); // Ip-Adresse
+	server_addr.sin_port = htons(port); // Port
+  if (sendto(socket, out_buf, out_len, MSG_CONFIRM, (struct sockaddr*) &server_addr, slen) == -1)
+    return -4;
+  if ((in_len = recvfrom(socket, in_buf, in_buf_len, MSG_WAITALL, (struct sockaddr *) &server_addr, &slen)) == -1)
+    return -3;
+  return in_len;
 }
--- source/xns/xns.c
+++ source/xns/xns.c
@@ -5,10 +5,25 @@
 #include "../include/soclib.h"
 
 #define PORT 53
+#define FORWARD 0x01010101
 #define BUFLEN 512
 #define MAX_DOMAIN_LENGTH 64
+#define CLASS_IN 1
+#define RECORD_A 1
+
+typedef struct recordA {
+  char *domain;
+  uint32_t ipv4;
+  uint16_t ttl;
+} recordA_t;
+
+recordA_t recordsA[] = {
+  {"me.intern", 0x7F000001, 3600}
+};
+int num_recordsA = 1;
 
 char domain[MAX_DOMAIN_LENGTH];
+int forward_socket;
 
 char *parse_domain(char *buf_ptr) {
   char * domain_ptr = domain;
@@ -22,47 +37,53 @@ char *parse_domain(char *buf_ptr) {
   return buf_ptr + 1;
 }
 
-char *write_uint16(char *ptr, uint16_t value){
-  ptr[0] = (char) (value>>8);
-  ptr[1] = (char) value;
-  return ptr+2;
-}
-char *write_uint32(char *ptr, uint32_t value){
-  ptr[0] = (char) (value>>24);
-  ptr[1] = (char) (value>>16);
-  ptr[2] = (char) (value>>8);
-  ptr[3] = (char) value;
-  return ptr+4;
-}
-char *read_uint16(char *ptr, uint16_t *value) {
-  *value = (((uint16_t)(ptr[0]))<<8) | (uint16_t)(ptr[1]);
-  return ptr+2;
+int lookupA(char *domain, uint32_t *ipv4, uint16_t *ttl) {
+  for (int i = 0; i < num_recordsA; ++i) {
+    if (!strcmp(recordsA[i].domain, domain)) {
+      *ipv4 = recordsA[i].ipv4;
+      *ttl = recordsA[i].ttl;
+      return 0;
+    }
+  }
+  return 1;
 }
 
 int dns_handler(char *in_buf, char **out_buf, int in_len) {
-  char *ptr = in_buf+2;
+  // check qcount and forward, if not 1
+  uint16_t qcount;
+  read_uint16(in_buf+4, &qcount);
+  if (qcount != 1)
+    return udp_request(forward_socket, 53, FORWARD, in_buf, in_len, in_buf, BUFLEN);
+
+  // parse question and forward if not CLASS_IN and RECORD_A
+  char *ptr = in_buf+12;
   uint16_t type, class;
-
-  // write header
-  ptr = write_uint16(ptr, 0x8180);      // FLAGS
-  ptr = write_uint16(ptr, 1);           // QCOUNT
-  ptr = write_uint16(ptr, 1);           // ACOUNT
-  ptr = write_uint16(ptr, 0);           // NSCOUNT
-  ptr = write_uint16(ptr, 0);           // ARCOUNT
-
-  // read question body
   ptr = parse_domain(ptr);              // read domain
-  ptr = read_uint16(ptr, &type);        // read type
-  ptr = read_uint16(ptr, &class);       // read class
-  printf("domain: %s\n", domain);
+  ptr = read_uint16(ptr, &type);        // read type (A)
+  ptr = read_uint16(ptr, &class);       // read class (IN)
+  if (class != CLASS_IN || type != RECORD_A)
+    return udp_request(forward_socket, 53, FORWARD, in_buf, in_len, in_buf, BUFLEN);
+
+  // lookup and forward if not found in database
+  uint32_t ipv4;
+  uint16_t ttl;
+  if (lookupA(domain, &ipv4, &ttl))
+    return udp_request(forward_socket, 53, FORWARD, in_buf, in_len, in_buf, BUFLEN);
 
   // write answer body
   ptr = write_uint16(ptr, 0xc00c);      // write pointer/offset (c|00c)
   ptr = write_uint16(ptr, type);        // write type
   ptr = write_uint16(ptr, class);       // write class
-  ptr = write_uint32(ptr, 3600);        // write time to live (1h)
+  ptr = write_uint32(ptr, ttl);         // write time to live (1h)
   ptr = write_uint16(ptr, 4);           // write length of answer (4 Byte for IPv4)
-  ptr = write_uint32(ptr, 0x7F000001);  // write IPv4 address (127.0.0.1)
+  ptr = write_uint32(ptr, ipv4);        // write IPv4 address (127.0.0.1)
+
+  // write header
+  write_uint16(in_buf+2,  0x8180);      // FLAGS
+  write_uint16(in_buf+4,  1);           // QCOUNT
+  write_uint16(in_buf+6,  1);           // ACOUNT
+  write_uint16(in_buf+8,  0);           // NSCOUNT
+  write_uint16(in_buf+10, 0);           // ARCOUNT
 
   // return length of answer
   return ptr-in_buf;
@@ -70,5 +91,8 @@ int dns_handler(char *in_buf, char **out_buf, int in_len) {
 
 int main(int argc, char *argv[]) {
   char buf[BUFLEN];
+	if ((forward_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    return -1;
   return udp_server(PORT, buf, BUFLEN, dns_handler);
+  close(forward_socket);
 }
